d8238fe1d59a8a33c4340b68517589af32e399c1
Zade <132782700+ZadeAmmar@users.noreply.github.com>
2023-09-27 12:58:03 -0400

    Time: 24 ms (36.36%), Space: 13.1 MB (94.95%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -7,10 +7,12 @@ class Solution(object):
         if x == 0 or x == 1:
             return x
 
-        start, end = 0, x // 2
+        start = 0
+        end = x // 2
         answer = 0
 
         while start <= end:
+            
             mid = (start + end) // 2 
 
             if mid * mid == x:
