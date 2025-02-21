ddc2153593f45a9ac222cc05706a4f1c962ba21f
Zade <132782700+ZadeAmmar@users.noreply.github.com>
2025-02-20 21:54:59 -0500

    Delete 0028-find-the-index-of-the-first-occurrence-in-a-string directory

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def strStr(self, haystack, needle):
-        for i in range(len(haystack)):
-            if haystack[i:i+len(needle)] == needle:
-                return i
-        return -1
-        
