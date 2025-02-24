6e33349cc77ee195e29919b1a675a97cc092c511
Zade <132782700+ZadeAmmar@users.noreply.github.com>
2025-02-24 13:46:43 -0500

    Delete 0070-climbing-stairs directory

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++
@@ -1,7 +0,0 @@
-class Solution(object):
-    def climbStairs(self, n):
-        array = [1]*(n+1)
-        for i in range(2, n+1):
-            array[i] = array[i-1]+array[i-2]
-        return array[n]
-        
